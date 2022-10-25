4376c0917a45602c2fb868c3c792a8bfc07be8a3
pilipenkoliza <116028427+pilipenkoliza@users.noreply.github.com>
2022-10-25 13:02:36 +0300

    Create Laba_3.cpp

---
+++ Лиза Пилипенко/Laba_3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+
+void kvadrat(double a, double b, double c) {
+
+    double x;
+    int D;
+
+    D = b * b - 4 * a * c;
+
+    if (D > 0)
+    {
+        x = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
+        cout << "Первый корень равен " << x << endl;
+
+        x = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
+        cout << "Второй корень равен " << x << endl;
+    }
+    if (D == 0)
+    {
+        x = (-1 * b) / (2 * a);
+        cout << "Корень равен " << x << endl;
+    }
+    if (D < 0)
+    {
+        cout << "Дискриминант меньше 0, корни невещественные." << endl;
+    }
+    
+}
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите значение a: ";
+    cin >> a;
+    cout << "Введите значение b: ";
+    cin >> b;
+    cout << "Введите значение c: ";
+    cin >> c;
+
+    kvadrat(a ,b ,c);
+
+    return 0;
+}
