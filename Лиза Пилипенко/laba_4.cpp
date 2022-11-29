922218c7dafbee62753aefbe65b027e82c55282b
pilipenkoliza <116028427+pilipenkoliza@users.noreply.github.com>
2022-11-29 18:36:34 +0300

    Create laba_4.cpp

---
+++ Лиза Пилипенко/laba_4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void swape(int& a, int& b) {
+    int buffer = a;
+    a = b;
+    b = buffer;
+}
+
+void shuffle(int* mass, int N)
+{
+    for (int i = 0; i < N; ++i)
+        swape(mass[i], mass[rand ()% N]);
+}
+ 
+int main(){
+    int N;
+    cout << "Array size: " << endl;
+    cin >> N;
+    int* mass = new int[N];
+    cout << "Array elements: " << endl;
+    for (int r = 0; r < N; r++) {
+        cin >> mass[r];
+    }
+    std::cout << std::endl;
+    srand(time(NULL));
+
+    shuffle(mass, N);
+    for (int j = 0; j < N; ++j)
+        cout << mass[j];
+
+    return 0;
+}
